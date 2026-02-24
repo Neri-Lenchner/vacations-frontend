45438b72d84401ad9b1418d396ad886df0ae7378
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-02-24 12:12:14 +0200

    changed general css

--- src/utils/Routing.tsx
+++ src/utils/Routing.tsx
@@ -1,17 +1,16 @@
 import {JSX} from "react";
 import {Route, Routes} from "react-router-dom";
 import Main from "../component/layout/main/Main";
-import CourseForm from "../component/course-form/CourseForm";
-import CourseListRoute from "../component/course-list-route/CourseListRoute";
+
 
 
 function Routing(): JSX.Element {
     return (
         <Routes>
-            <Route path="/courses-list" element={<CourseListRoute />}/>
-            <Route path="/new-course/:id?" element={<CourseForm />}/>
-            <Route path="/" element={<Main />}/>
-            <Route path="*" element={<Main />}/>
+            <Route path="/courses-list" element="example:{<CourseListRoute />}"/>
+            <Route path="/new-course/:id?" element="example:{<CourseForm />}"/>
+            <Route path="/" element={<Main />} />
+            <Route path="*" element={<Main />} />
         </Routes>
     );
 }
