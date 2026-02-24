7e834f3e83f33eaa6eca92a6f3ae88cf495e572c
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-02-24 11:48:00 +0200

    moved utils into src dir

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {JSX} from 'react';
 import './App.css';
 import Header from "./component/layout/header/Header";
 import SideBar from "./component/layout/side-bar/SideBar";
-import Routing from "../utils/Routing";
+import Routing from "./utils/Routing";
 
 function App(): JSX.Element {
 
