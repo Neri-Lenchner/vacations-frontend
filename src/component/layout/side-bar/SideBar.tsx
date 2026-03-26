559df70fd736cdc27b458f6c87ef4f27c627f482
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-03-26 15:39:00 +0200

    first commit

--- src/component/layout/side-bar/SideBar.tsx
+++ src/component/layout/side-bar/SideBar.tsx
@@ -7,11 +7,11 @@ function SideBar(): JSX.Element {
     return (
         <div className="SideBar">
           <div className="links">
-             <NavLink to="/contact-us">Contact Us</NavLink>
-              <NavLink to="/about-us">About</NavLink>
-              <NavLink to="/services">Services</NavLink>
-              <NavLink to="/task-list">Task List</NavLink>
-              <NavLink to="/test-params">Test Params</NavLink>
+              <NavLink to="/">Link</NavLink>
+              <NavLink to="/">Link</NavLink>
+              <NavLink to="/">Link</NavLink>
+              <NavLink to="/">Link</NavLink>
+              <NavLink to="/">Link</NavLink>
           </div>
         </div>
     );
--- src/utils/app-config.ts
+++ src/utils/app-config.ts
@@ -3,11 +3,11 @@ class AppConfig {
 }
 
 class DevAppConfig extends AppConfig {
-    apiAddress: string = "https://67b34380392f4aa94fa6893d.mockapi.io/api/"; // localhost/mockapi.io/api/
+    apiAddress: string = "http://localhost:5000"; // localhost/mockapi.io/api/
 }
 
 class ProdAppConfig extends AppConfig {
-    apiAddress: string = "https://67b34380392f4aa94fa6893d.mockapi.io/api/";
+    apiAddress: string = "http://localhost:5000";
 }
 
 export const appConfig = process.env.NODE_ENV === "production" ? new ProdAppConfig() : new DevAppConfig;
