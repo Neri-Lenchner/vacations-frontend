58b04c4f8c518dcc7c9630321292d636ac405c1b
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-03-31 13:58:15 +0300

    added userService.ts

---
+++ src/services/user-service.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import {appConfig} from "../utils/app-config";
+import {authStore} from "../state/auth-state";
+import {User} from "../models/user";
+import {UserActionType, userStore} from "../state/user-state";
+
+class UserService {
+
+    isFetched: boolean = false;
+
+    public async getUserList(forceFetch: boolean = false): Promise<User[]> {
+        if (!this.isFetched || forceFetch) {
+            try {
+                const response = await axios.get<User[]>(appConfig.apiAddress + "user/", {headers: {Authorization: "Bearer " + authStore.getState().token}});
+                userStore.dispatch({type: UserActionType.GetUserList, payload: response.data});
+            } catch (err) {
+                console.error("Error from getUserList");
+                throw err;
+            }
+        }
+        return userStore.getState().userList;
+    }
+
+}
+
+export const userService = new UserService();
