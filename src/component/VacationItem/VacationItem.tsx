4db1d9ce041f068724cd9d16f82ee8eba67c47eb
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-04-03 00:21:22 +0300

    set VacationItem

--- src/component/VacationItem/VacationItem.tsx
+++ src/component/VacationItem/VacationItem.tsx
@@ -1,7 +1,19 @@
 import "./VacationItem.css";
-
+import {Vacation} from '../../models/vacation.model';
 
 function VacationItem() {
+
+    const vacation: Vacation = new Vacation(
+        "Los Angeles",
+        "Discover the vibrant energy of Los Angeles, where sun-soaked beaches, iconic landmarks, and world-class entertainment await. Stroll along Hollywood Boulevard, relax in Santa Monica, and explore trendy neighborhoods filled with culture, cuisine, and creativity. From breathtaking coastal views to unforgettable nightlife, LA offers the perfect blend of excitement and relaxation for your ultimate getaway experience today. Book and create lasting memories in this dazzling California destination.",
+        "2026-10-03 00:00:00",
+        "2026-11-03 00:00:00",
+        10000
+    );
+
+    vacation.startDate = vacation.startDate.split(' ')[0].split('-').reverse().join('.');
+    vacation.endDate = vacation.endDate.split(' ')[0].split('-').reverse().join('.');
+
     return (
         <div className="vacation-item-container">
             <div className="vacation-item-image-container">
@@ -13,18 +25,19 @@ function VacationItem() {
                     <line x1="3" y1="10" x2="21" y2="10"/>
                     <rect x="16" y="16" width="4" height="4"/>
                 </svg>
-                20 10 2022 - 9 11-2022
+                {/*20 10 2022 - 9 11-2022*/}
+                {vacation.startDate} - {vacation.endDate}
             </div>
             <div className="vacation-item-content-container">
                 <p className="vacation-item-text">
-                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)
+                    {vacation.description}
                 </p>
                 <button className="form-button vacation-item-button">
-                    $ Price
+                    $ {vacation.cost}
                 </button>
             </div>
             <div className="vacation-item-title">
-                Vacation
+                {vacation.destination}
             </div>
             <div className="vacation-item-likes-container">
                 <div className="vacation-item-likes-container-content">
