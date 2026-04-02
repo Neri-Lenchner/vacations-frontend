8ce74e0bbce6b4c0522684778d0811a10621ad79
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-04-02 11:41:26 +0300

    added vacationService and vacation.model

---
+++ src/models/vacation.model.ts
@@ -0,0 +1,18 @@
+export class Vacation {
+    public destination: string;
+    public description: string;
+    public startDate: string;
+    public endDate: string;
+    public cost: number;
+    public image?: string;
+    public id?: number;
+    constructor(vacation: Vacation) {
+        this.destination = vacation.destination;
+        this.description = vacation.description;
+        this.startDate = vacation.startDate;
+        this.endDate = vacation.endDate;
+        this.cost = vacation.cost;
+        this.image = vacation.image;
+        this.id = vacation.id;
+    }
+}
