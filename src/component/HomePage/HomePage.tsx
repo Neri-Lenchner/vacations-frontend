aa47ffca44e373b137b1517424e29bed938e68ae
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-04-19 01:20:14 +0300

    starting a animation tryout

--- src/component/HomePage/HomePage.tsx
+++ src/component/HomePage/HomePage.tsx
@@ -3,7 +3,9 @@ import './HomePage.css'
 function HomePage() {
     return (
         <div className="home-page-container">
-            Home Page
+            <p className="home-page-text">
+                TraveLentz is an innovative travel agency that creates personalized journeys tailored to every traveler’s interests. It blends adventure, culture, and comfort to deliver memorable experiences across the globe. From serene beaches to bustling cities, each itinerary is carefully crafted. TraveLentz emphasizes sustainability and meaningful local connections, ensuring responsible travel while helping clients discover the authentic beauty and spirit of every destination.
+            </p>
         </div>
     );
 }
