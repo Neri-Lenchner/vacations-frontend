90ac00c1c222f04c6b0adfec34a015fc8254799f
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-04-19 06:18:14 +0300

    chart in progress

--- src/component/Cahrts/Charts.tsx
+++ src/component/Cahrts/Charts.tsx
@@ -1,9 +1,25 @@
+import {Chart as ChartsJS, defaults} from 'chart.js/auto';
+import {Radar, Line, Bar, Doughnut, Pie} from 'react-chartjs-2';
+import {data} from "react-router-dom";
+import {vacationStore} from '../../state/vacation-state';
 import "./Charts.css";
+
+
 
 function Charts() {
     return (
         <div className="Charts-container">
-            Charts
+           {/*<Bar*/}
+           {/*     data={{*/}
+           {/*         labels: vacationStore.getState().vacationList.map(vacation => vacation.destination),*/}
+           {/*         datasets: [*/}
+           {/*             {*/}
+           {/*                 label: "Vacations",*/}
+           {/*                 data: vacationStore.getState().vacationList.map(vacation => vacation.destination)*/}
+           {/*             }*/}
+           {/*         ]*/}
+           {/*     }}*/}
+           {/*/>*/}
         </div>
     );
 }
