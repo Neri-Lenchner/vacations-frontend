9e31b68d6d5fa2f42045e92fa6098ae83d15f14f
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-04-27 14:24:16 +0300

    commented out SideBar.tsx and SideBar.css and Footer.tsx too

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, {JSX} from 'react';
 import './App.css';
 import Header from "./component/layout/header/Header";
-import SideBar from "./component/layout/side-bar/SideBar";
 import Routing from "./utils/Routing";
 
 function App(): JSX.Element {
@@ -12,9 +11,6 @@ function App(): JSX.Element {
                 <Header/>
             </header>
             <section className="content">
-                {/*<aside>*/}
-                {/*    <SideBar/>*/}
-                {/*</aside>*/}
                 <main>
                     <Routing/>
                 </main>
