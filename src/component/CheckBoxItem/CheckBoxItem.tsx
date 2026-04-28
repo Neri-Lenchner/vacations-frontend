5267098643c055d521d21330af99022d51796242
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-04-28 19:14:17 +0300

    added CheckBoxItem

---
+++ src/component/CheckBoxItem/CheckBoxItem.tsx
@@ -0,0 +1,12 @@
+import {JSX} from 'react';
+import './CheckBoxItem.css';
+
+function CheckBoxItem(): JSX.Element {
+    return (
+        <div>
+            CheckBoxItem
+        </div>
+    );
+}
+
+export default CheckBoxItem;
